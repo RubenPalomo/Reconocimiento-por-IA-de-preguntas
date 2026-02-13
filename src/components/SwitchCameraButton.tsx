294dfcc6ea2b88567d5208f538a6be4444545703
RubenPalomo-Tria <ruben.palomof@gmail.com>
2026-02-13 19:16:34 +0100

    Update SwitchCameraButton.tsx

--- src/components/SwitchCameraButton.tsx
+++ src/components/SwitchCameraButton.tsx
@@ -7,7 +7,7 @@ export default function SwitchCameraButton({
 }) {
   return (
     <button
-      className="absolute right-6 bottom-6 rounded-full bg-white/10 p-3 hover:bg-white/20"
+      className="fixed right-6 bottom-6 rounded-full bg-white/10 p-3 hover:bg-white/20"
       onClick={switchCamera}
     >
       <SwitchCamera />
